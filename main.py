c6de4cc6777afde874b75fc78a7d82f8f1c8e30c
Vojtěch Musílek <vojtechmusilekk@gmail.com>
2024-03-14 15:05:27 +0100

    commit test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import os
 import dotenv
 from datetime import date
-from phable.client import Client
-from phable.kinds import DateRange, Grid,Ref,Number
+from phable.client import Client, CommitFlag
+from phable.kinds import DateRange, Grid, Ref, Number, Marker
 
 dotenv.load_dotenv()
 uri = os.environ["URI"]
@@ -11,24 +11,32 @@ username = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
 
 with Client(uri, username, password) as client:
-  points = client.read('point and tz=="Prague" and (unit == "kW" or unit == "°C")', 2)
-  grid = client.his_read(points, DateRange(date(2020, 1, 1), date(2020, 2, 1)))
-  df = grid.to_pandas()
-  df.dropna(inplace=True)
-  dis_to_cn = {str(col_grid["meta"]["id"]): col_grid["name"] for col_grid in filter(lambda x: x["name"] != "ts", grid.cols)}
-  df.rename(columns=dis_to_cn, inplace=True)
+    data = [{"dis": "TestRec", "testing": Marker(), "pytest": Marker()}]
+    response: Grid = client.commit(data, CommitFlag.ADD, False)
 
-  df["prediction"] = df["v0"] + df["v1"]
+    exit()
 
-  meta = {"ver": "3.0", "id": Ref("p:energyTwinForge:r:2d44cd91-c8fd065c")}
-  cols = [{"name": "ts"}, {"name": "val"}]
-  rows = []
-  for index, row in df.iterrows():
-      rows.append({"ts": index.to_pydatetime(), "val": Number(row['prediction'])})
-  grid = Grid(meta, cols, rows)
-  client.his_write(grid)
+    points = client.read('point and tz=="Prague" and (unit == "kW" or unit == "°C")', 2)
+    grid = client.his_read(points, DateRange(date(2020, 1, 1), date(2020, 2, 1)))
+    df = grid.to_pandas()
+    df.dropna(inplace=True)
+    dis_to_cn = {
+        str(col_grid["meta"]["id"]): col_grid["name"]
+        for col_grid in filter(lambda x: x["name"] != "ts", grid.cols)
+    }
+    df.rename(columns=dis_to_cn, inplace=True)
 
-  #client.eval
+    df["prediction"] = df["v0"] + df["v1"]
 
-  print(df)
-  print(df.info())
+    meta = {"ver": "3.0", "id": Ref("p:energyTwinForge:r:2d44cd91-c8fd065c")}
+    cols = [{"name": "ts"}, {"name": "val"}]
+    rows = []
+    for index, row in df.iterrows():
+        rows.append({"ts": index.to_pydatetime(), "val": Number(row["prediction"])})
+    grid = Grid(meta, cols, rows)
+    client.his_write(grid)
+
+    # client.eval
+
+    print(df)
+    print(df.info())
